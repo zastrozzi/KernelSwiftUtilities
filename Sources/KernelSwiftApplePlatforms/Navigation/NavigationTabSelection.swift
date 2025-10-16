88f7fd4c65231791ff3b7fd75e4fd954d3922396
Jono Forbes <jono@zastrozzi.com>
2025-10-17 00:41:46 +0200

    add NavigationTabSelection

---
+++ Sources/KernelSwiftApplePlatforms/Navigation/NavigationTabSelection.swift
@@ -0,0 +1,25 @@
+//
+//  File.swift
+//  KernelSwiftUtilities
+//
+//  Created by Jonathan Forbes on 17/10/2025.
+//
+
+import Foundation
+import SwiftUI
+
+public protocol NavigationTabSelection: RawRepresentable, Identifiable, Codable, Equatable, Hashable, CaseIterable where RawValue == String {
+    var title: String { get }
+    var systemImage: String { get }
+}
+
+extension NavigationTabSelection {
+    public var id: String { rawValue }
+}
+
+@available(iOS 18.0, macOS 15.0, tvOS 18.0, watchOS 11.0, visionOS 2.0, *)
+extension Tab {
+    nonisolated public init(value: Value, @ViewBuilder content: () -> Content) where Content: View, Label == DefaultTabLabel, Value: NavigationTabSelection {
+        self.init(value.title, systemImage: value.systemImage, value: value, content: content)
+    }
+}
